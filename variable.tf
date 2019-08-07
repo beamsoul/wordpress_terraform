d4b35de3edb240fc578ee9fee0e1f8b14551485d
Madina89 <madinalinux89@gmail.com>
2019-08-07 14:06:02 -0500

    variable added

---
+++ variable.tf
@@ -0,0 +1,31 @@
+
+variable "Name" {
+
+}
+
+variable "Env" {
+
+}
+
+variable "Created_by" {
+
+}
+
+variable "Dept" {
+
+}
+
+variable "ami" {
+
+}
+
+variable "key_name" {
+
+}
+variable "instance_type" {
+
+}
+
+variable "region" {
+
+}
