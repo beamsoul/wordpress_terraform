f0ed469ad179cdf2ecfe23488e72afbb04c6fc4f
Madina89 <madinalinux89@gmail.com>
2019-08-07 17:29:28 -0500

    updated

--- istance.tf
+++ istance.tf
@@ -1,10 +1,10 @@
 
-resource "aws_instance" "web1" {
+resource "aws_instance" "wordpress" {
 
   ami                         = "${var.ami}"
   instance_type               = "${var.instance_type}"
   key_name                    = "${var.key_name}"
-  subnet_id                   = "${aws_subnet.public-a.id}"
+  subnet_id                   = "${aws_subnet.public-sd.id}"
   vpc_security_group_ids      = ["${aws_security_group.public.id}"]
   associate_public_ip_address = "true"
   user_data                   = "${file("userdata.sh")}"
--- s_aasociation.tf
+++ s_aasociation.tf
@@ -1,5 +1,5 @@
 resource "aws_route_table_association" "a" {
-  subnet_id      = "${aws_subnet.public.id}"
+  subnet_id      = "${aws_subnet.public-sd.id}"
   route_table_id = "${aws_route_table.r.id}"
 }
 
