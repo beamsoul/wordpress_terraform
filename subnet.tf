43855116262359de273a06472f42eba6112736f7
Madina89 <madinalinux89@gmail.com>
2019-08-07 17:16:12 -0500

    update

--- subnet.tf
+++ subnet.tf
@@ -1,4 +1,4 @@
-resource "aws_subnet" "public" {
+resource "aws_subnet" "public-sd" {
   vpc_id     = "${aws_vpc.dev.id}"
   cidr_block = "10.0.1.0/24"
 
@@ -9,7 +9,7 @@ resource "aws_subnet" "public" {
     Dept      = "${var.Dept}"
   }
 }
-resource "aws_subnet" "public" {
+resource "aws_subnet" "public-sb" {
   vpc_id     = "${aws_vpc.dev.id}"
   cidr_block = "10.0.2.0/24"
 
@@ -20,7 +20,7 @@ resource "aws_subnet" "public" {
     Dept      = "${var.Dept}"
   }
 }
-resource "aws_subnet" "public" {
+resource "aws_subnet" "public-gt" {
   vpc_id     = "${aws_vpc.dev.id}"
   cidr_block = "10.0.3.0/24"
 
@@ -35,7 +35,7 @@ resource "aws_subnet" "public" {
 
 
 #This will be private subnets
-resource "aws_subnet" "private" {
+resource "aws_subnet" "private-sb" {
   vpc_id     = "${aws_vpc.dev.id}"
   cidr_block = "10.0.11.0/24"
 
@@ -46,7 +46,7 @@ resource "aws_subnet" "private" {
     Dept      = "${var.Dept}"
   }
 }
-resource "aws_subnet" "private" {
+resource "aws_subnet" "private-gt" {
   vpc_id     = "${aws_vpc.dev.id}"
   cidr_block = "10.0.12.0/24"
 
@@ -57,7 +57,7 @@ resource "aws_subnet" "private" {
     Dept      = "${var.Dept}"
   }
 }
-resource "aws_subnet" "private" {
+resource "aws_subnet" "private-hy" {
   vpc_id     = "${aws_vpc.dev.id}"
   cidr_block = "10.0.13.0/24"
 
